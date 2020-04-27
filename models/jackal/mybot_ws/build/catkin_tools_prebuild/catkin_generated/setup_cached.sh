9cd6f7246628daedf0e3f336e2014083a73b1e73
Kem98m <46077117+Kem98m@users.noreply.github.com>
2020-04-27 15:48:51 -0400

    Add files via upload

---
+++ models/jackal/mybot_ws/build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/himanshu/mybot_ws/devel/.private/catkin_tools_prebuild:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu"
+export PKG_CONFIG_PATH="/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig"
+export PWD="/home/himanshu/mybot_ws/build/catkin_tools_prebuild"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/himanshu/mybot_ws/devel/.private/catkin_tools_prebuild/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/himanshu/mybot_ws/build/catkin_tools_prebuild:/home/himanshu/mybot_ws/src/mybot_control:/home/himanshu/mybot_ws/src/mybot_description:/home/himanshu/mybot_ws/src/mybot_gazebo:/home/himanshu/mybot_ws/src/mybot_navigation:/opt/ros/kinetic/share"
